6715afbfeaad87f247dde0c4e5f9e75dfcbc2763
AlexaFoxKhai <alexababiy118@gmail.com>
2016-12-11 14:15:29 +0200

    Create Zapusk.java

---
+++ Zapusk.java
@@ -0,0 +1,12 @@
+package labw04babiy;
+import javax.swing.*;
+
+public class Zapusk {
+    public static void main(String args[]){
+        
+        Okno mainFrame = new Okno("Работа No4. Бабий");
+        mainFrame.setSize(300, 200);
+        mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        mainFrame.setVisible(true);
+    }
+}
